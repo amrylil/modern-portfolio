b83bae3171a076080f51afc4d32353903d3e23b3
AmryLil <ulilamry432@gmail.com>
2025-11-05 20:49:38 +0800

    fix: animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { HomeSection } from "@/components/sections/HomeSection";
 import { ProjectSection } from "@/components/sections/ProjectSection";
 import { AboutSection } from "@/components/sections/AboutSection";
-import FeaturesSectionDemo from "@/components/features-section-demo-1";
 import ReviewSection from "@/components/sections/ReviewSection";
 
 export default function Home() {
